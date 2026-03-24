a6242de33dda075fbb534730f1cf22c6fb252a9c
mithereal <mithereal@gmail.com>
2026-03-24 03:44:07 -0700

    feat: add endpoint versioning and health

--- lib/framework_web/endpoint.ex
+++ lib/framework_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule FrameworkWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
+    websocket: [connect_info: [session: {__MODULE__, :session_opts, []}]],
+    longpoll: [connect_info: [session: {__MODULE__, :session_opts, []}]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
@@ -23,7 +23,8 @@ defmodule FrameworkWeb.Endpoint do
     at: "/",
     from: :framework,
     gzip: false,
-    only: FrameworkWeb.static_paths()
+    only: FrameworkWeb.static_paths(),
+    headers: {__MODULE__, :static_headers, []}
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
@@ -46,8 +47,68 @@ defmodule FrameworkWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug Sentry.PlugContext
   plug Plug.MethodOverride
   plug Plug.Head
-  plug Plug.Session, @session_options
+  plug :session
+  plug :health
+  plug :version
+  # plug Plug.Session, @session_options
   plug FrameworkWeb.Router
+
+  def session(conn, _opts) do
+    opts = session_opts()
+
+    Plug.Session.call(conn, Plug.Session.init(opts))
+  end
+
+  def session_opts do
+    Keyword.put(@session_options, :domain, FrameworkWeb.Endpoint.host())
+  end
+
+  def health(conn, _) do
+    case conn do
+      %{request_path: "/health"} -> conn |> send_resp(200, "OK") |> halt()
+      _ -> conn
+    end
+  end
+
+  def version(conn, _) do
+    case conn do
+      %{request_path: "/version"} ->
+        data = Framework.Application.version() |> Map.new() |> Jason.encode!()
+        conn |> put_resp_content_type("application/json") |> send_resp(200, data) |> halt()
+
+      %{request_path: "/version/build"} ->
+        [app: name] = Framework.Application.name()
+        [version: version] = Framework.Application.version()
+        [description: description] = Framework.Application.description()
+        [build_date: build_date] = Framework.Application.build_date()
+        [build_hash: build_hash] = Framework.Application.build_hash()
+
+        data =
+          Map.new()
+          |> Map.put(:application, %{
+            name: name,
+            version: version,
+            description: description,
+            date: build_date,
+            revision: build_hash
+          })
+          |> Map.put(:runtime, System.build_info())
+          |> Jason.encode!()
+
+        conn |> put_resp_content_type("application/json") |> send_resp(200, data) |> halt()
+
+      _ ->
+        conn
+    end
+  end
+
+  def static_headers(conn) do
+    case conn.request_path do
+      "/assets/serviceworker.js" -> [{"Service-Worker-Allowed", "/"}]
+      _ -> []
+    end
+  end
 end
